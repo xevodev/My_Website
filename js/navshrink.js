027b650831489b397d2ee484d649fd0ab936179c
Damian <damcio197@gmail.com>
2018-10-14 20:57:10 +0200

    Navbar v2.0 + sekcje

--- js/navshrink.js
+++ js/navshrink.js
@@ -2,13 +2,9 @@ $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 10) {
       $('nav').addClass('shrink');
-      $('nav').removeClass('navbar-dark');
-      $('nav').addClass('navbar-light');
     }
     else {
     $('nav').removeClass('shrink');
-    $('nav').removeClass('navbar-light');
-    $('nav').addClass('navbar-dark');
     }
   });
 });
