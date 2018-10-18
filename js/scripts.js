474c13f4f9eda8fcb1f3ae929eaf65260307e74b
Damian <damcio197@gmail.com>
2018-10-18 10:53:43 +0200

    Upublicznienie strony + poprawki

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Changes color of navbar when scrolled
+$(document).ready(function() {
+    if($(document).scrollTop() > 10) {
+        $('nav').addClass('shrink');
+        }
+        else {
+        $('nav').removeClass('shrink');
+        }
+        $(window).scroll(function() {
+        if($(document).scrollTop() > 10) {
+        $('nav').addClass('shrink');
+        }
+        else {
+        $('nav').removeClass('shrink');
+        }
+    });
+}); 
+
+//Smoothly scrolls to sections
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('.collapse').collapse('hide');
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 60
+    }, 500);
+});
